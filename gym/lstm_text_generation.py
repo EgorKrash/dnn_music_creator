4a63610aa48473af1dd51a20100cf8ecc53a51c7
TIXFeniks <dimdi-y@ya.ru>
2016-08-27 14:25:38 +0300

    Added note durations, note octaves

--- gym/lstm_text_generation.py
+++ gym/lstm_text_generation.py
@@ -20,7 +20,7 @@ from os import listdir
 
 from music21 import converter
 
-maxlen = 100
+maxlen = 200
 step = 3
 
 text = ''
@@ -35,7 +35,6 @@ def dur_to_text(dur):
     else:
         return _dur_to_text['quarter']
 
-
 for thisNote in parsed.recurse().notes:
     for pitch in thisNote.pitches:
         text += pitch.name+str(pitch.octave)
@@ -43,17 +42,12 @@ for thisNote in parsed.recurse().notes:
 
 print('corpus length:', len(text))
 
-#chars = sorted(list(set(text)))
-
-chars = sorted(list(set('ABCDEFG1234567890lkjhpoiu-#zy')))
-
-print(chars)
-
+chars = sorted(list(set('ABCDEFGlkjhpoiuyz1234567890#-')))
 print('total chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
-# cut the text in semi-redundant sequences of maxlen characters
+generate_per_batch = 100
 
 sentences = []
 next_chars = []
@@ -71,12 +65,12 @@ for i, sentence in enumerate(sentences):
     y[i, char_indices[next_chars[i]]] = 1
 
 print ('Gettng data')
-def generate_text():
+def generate_text(chars):
     while True:
         files = [each for each in listdir('midi') if each.endswith('.mid')]
         for i, fi in enumerate(files):
             text = ''
-            print(fi)
+            #print(fi)
             parsed = converter.parse('midi/'+fi)
 
             for thisNote in parsed.recurse().notes:
@@ -84,7 +78,7 @@ def generate_text():
                     text += pitch.name + str(pitch.octave)
                 text += dur_to_text(thisNote.duration.type) + 'z'
 
-            print('corpus length:', len(text))
+            #print('corpus length:', len(text))
             # cut the text in semi-redundant sequences of maxlen characters
 
             sentences = []
@@ -92,23 +86,28 @@ def generate_text():
             for i in range(0, len(text) - maxlen, step):
                 sentences.append(text[i: i + maxlen])
                 next_chars.append(text[i + maxlen])
-            print('nb sequences:', len(sentences))
+            #print('nb sequences:', len(sentences))
 
             #print('Vectorization...')
-            X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-            y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+            X = np.zeros((generate_per_batch, maxlen, len(chars)), dtype=np.bool)
+            y = np.zeros((generate_per_batch, len(chars)), dtype=np.bool)
+            from_batch = 0
             for i, sentence in enumerate(sentences):
                 for t, char in enumerate(sentence):
-                    X[i, t, char_indices[char]] = 1
-                y[i, char_indices[next_chars[i]]] = 1
-            print (len(X[0]),len(y[0]))
-            yield (X,y)
+                    X[i%generate_per_batch, t, char_indices[char]] = 1
+                y[i%generate_per_batch, char_indices[next_chars[i]]] = 1
+                if i%generate_per_batch == generate_per_batch-1:
+                    yield ([X], [y])
+                    X = np.zeros((generate_per_batch, maxlen, len(chars)), dtype=np.bool)
+                    y = np.zeros((generate_per_batch, len(chars)), dtype=np.bool)
+
 
 
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(LSTM(512, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(LSTM(512 ))
 model.add(Dense(len(chars)))
 print (len(chars))
 model.add(Activation('softmax'))
@@ -116,10 +115,6 @@ model.add(Activation('softmax'))
 optimizer = RMSprop(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
-model.load_weights('text-model.h5py')
-
-for gen in generate_text():
-    print ('new')
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -131,37 +126,30 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # train the model, output generated text after each iteration
-for iteration in range(1, 1000):
+for iteration in range(1, 11):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit_generator(generate_text(), 1720, nb_epoch=1)
-    model.save_weights('text-model.h5py')
+    model.fit_generator(generate_text(chars), 1720, nb_epoch=20)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
-
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
-
         generated = ''
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
-
         for i in range(400):
             x = np.zeros((1, maxlen, len(chars)))
             for t, char in enumerate(sentence):
                 x[0, t, char_indices[char]] = 1.
-
             preds = model.predict(x, verbose=0)[0]
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
-
             generated += next_char
             sentence = sentence[1:] + next_char
-
             sys.stdout.write(next_char)
             sys.stdout.flush()
         print()
