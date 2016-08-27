60a9bf9a931cef9e940afed849f16ebe21aee920
EgorKrash <krasheninnikovegor@gmail.com>
2016-08-27 17:11:22 +0300

    finished text generator file

--- gym/generate_text.py
+++ gym/generate_text.py
@@ -1,4 +1,9 @@
 from text_model import *
+from getting_midi_from_string import StringConverter
 
 model.load_weights('text_model.h5py')
 
+
+converter = StringConverter(generate_song(1))
+
+converter.save('test.mid')
